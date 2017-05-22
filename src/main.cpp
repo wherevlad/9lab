babbd02be7039c77f3ccc4ba19cfbf1b6a4f76d8
wherevlad <where.vlad@gmail.com>
2017-05-22 16:49:46 +0700

    ok!

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,27 @@
 
 int main (){
 	int ye,date,d,m,c;
+	
+	
 	cout << "Введите сумму вклада(>=10000) и срок(<=365):\n";
-	cin >> ye;
-	if (ye<10){
-		cout << "Введите корректную сумму вклада..\n"; 
-		return 0;
-	};
-	cin >> date;
-	if ((date>365)||(date<=0)){
-		cout << "Введите корректное число срока вклада..\n"; 
-		return 0;
-	};
+	
+	//for(;;){
+		cin >> ye;
+		if (ye<10){
+			cout << "Введите корректную сумму вклада..\n"; 
+			return 0;
+			//break;
+		};
+	//}
+	
+	//for(;;){
+		cin >> date;
+		if ((date>365)||(date<=0)){
+			cout << "Введите корректное число срока вклада..\n"; 
+			return 0;
+			//break;
+		};
+	//}
 	
 	
 	if (ye<100) d=1;
@@ -22,5 +32,6 @@ int main (){
 	if ((date<=240) && (date>=121)) c=three(d,ye);
 	if ((date<=365) && (date>=241)) c=four(d,ye);
 	cout << "\n\n\nОтвет: " << c;
+	cout << endl;
 	return 0;
 }
